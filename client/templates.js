e25a6f5aa3ad122b98d239597fd535a725575ca4
ken <kenken17@gmail.com>
2015-06-02 22:10:34 +0800

    - add some fade out for closing the notification - sort notification by latest on top

--- client/templates.js
+++ client/templates.js
@@ -8,17 +8,53 @@ Template.MNotificationsTemplate.helpers({
     }
 });
 
+Template._MNotificationsDefault.onRendered(function() {
+	var EVENTS = 'webkitTransitionEnd oTransitionEnd transitionEnd msTransitionEnd transitionend';
+
+	this.firstNode._uihooks = {
+		//insertElement: function(node, next) {
+		//	$(node).addClass('off').insertBefore(next);
+		//
+		//	Tracker.afterFlush(function() {
+		//		$(node).removeClass('off');
+		//	});
+		//},
+
+		removeElement: function(node) {
+			$(node).addClass('off').on(EVENTS, function() {
+			    this.remove();
+			});
+		}
+	};
+});
+
 Template._MNotificationsDefault.helpers({
 	notifications: function() {
 		var notificationsServer = NotificationsCollection.find({
 			read: false
+		}, {
+			sort: {
+				createdAt: -1
+			}
 		}).fetch();
 
 		var notificationsClient = NotificationsClientCollection.find({
 			read: false
+		}, {
+			sort: {
+				createdAt: -1
+			}
 		}).fetch();
 
 		return _.union(notificationsClient, notificationsServer)
 	}
+});
+
+Template._MNotificationsDefault.events({
+    'click .btn-close-message': function(e) {
+        e.preventDefault();
+
+		MNotifications.read(this._id);
+    }
 });
 
